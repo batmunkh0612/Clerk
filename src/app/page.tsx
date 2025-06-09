cbf653335d6756b90cb76f9464ceabbc79e59184
Батмөнх Ариунболд <bgo6d@Bgo6d.local>
2025-06-09 16:43:11 +0800

    test

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="relative  w-full">
+      <div className="h-[200vh]"></div>;
+    </div>
+  );
+};
+export default Home;
